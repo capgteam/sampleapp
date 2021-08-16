cb311a3b443bf76bee5e775bebf2060c16734ff7
capgteam <capgteam22020@gmail.com>
2021-08-16 13:09:30 +0530

    First Commit!

---
+++ sampleapp/src/main/java/sampleapp/MainClass.java
@@ -0,0 +1,13 @@
+package sampleapp;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Good AfterNoon");
+		System.out.println("Have A Good Day!");
+		System.out.println("welcome to Git !");
+		
+	}
+
+}
