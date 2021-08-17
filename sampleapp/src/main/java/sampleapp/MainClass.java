f01646cf338b14cc888c2d9f5038e82e7829a322
capgteam <capgteam22020@gmail.com>
2021-08-17 11:30:08 +0530

    commit all

--- sampleapp/src/main/java/sampleapp/MainClass.java
+++ sampleapp/src/main/java/sampleapp/MainClass.java
@@ -7,6 +7,7 @@ public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Good AfterNoon");
 		System.out.println("Have A Good Day!");
+		System.out.println("welcome to Git !");
 	}
 	
 	
