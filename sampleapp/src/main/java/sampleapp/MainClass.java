c947b24d79950eec04b4df57f2a771b428560264
capgteam <capgteam22020@gmail.com>
2021-08-17 11:31:13 +0530

    commit delete1

--- sampleapp/src/main/java/sampleapp/MainClass.java
+++ sampleapp/src/main/java/sampleapp/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 	
 public static void main(String[] args) {
 		display();
-		System.out.println("Hello World!");
+		
 		System.out.println("Good AfterNoon");
 		System.out.println("Have A Good Day!");
 		System.out.println("welcome to Git !");
