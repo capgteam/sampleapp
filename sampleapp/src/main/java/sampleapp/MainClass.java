74cfc998e91a4bb13c126630264ab87dd8dddf31
capgteam <capgteam22020@gmail.com>
2021-08-17 11:16:35 +0530

    display method updated

--- sampleapp/src/main/java/sampleapp/MainClass.java
+++ sampleapp/src/main/java/sampleapp/MainClass.java
@@ -2,19 +2,15 @@ package sampleapp;
 
 public class MainClass {
 	
-	public static void greetUser() {
+public static void main(String[] args) {
+		
+	}
+	
+	public void display() {
 		System.out.println("Hello World!");
 		System.out.println("Good AfterNoon");
 		System.out.println("Have A Good Day!");
 		System.out.println("welcome to Git !");
-	}
-
-	public static void main(String[] args) {
-		
-		greetUser();
-	}
-	
-	public void display() {
 		
 	}
 
