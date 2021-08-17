a8a1948ae8503756818ccf6eb657bd09c88f46f9
capgteam <capgteam22020@gmail.com>
2021-08-17 11:16:55 +0530

    display method invoked

--- sampleapp/src/main/java/sampleapp/MainClass.java
+++ sampleapp/src/main/java/sampleapp/MainClass.java
@@ -7,6 +7,10 @@ public static void main(String[] args) {
 	}
 	
 	public void display() {
+		display();
+	}
+	
+	public static void display() {
 		System.out.println("Hello World!");
 		System.out.println("Good AfterNoon");
 		System.out.println("Have A Good Day!");
