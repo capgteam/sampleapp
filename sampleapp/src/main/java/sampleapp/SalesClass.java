42e2039f5b4d18fe790d16a40fde2965fc8800ca
capgteam <capgteam22020@gmail.com>
2021-08-17 10:14:58 +0530

    print method updated

--- sampleapp/src/main/java/sampleapp/SalesClass.java
+++ sampleapp/src/main/java/sampleapp/SalesClass.java
@@ -15,6 +15,9 @@ public class SalesClass {
 	
 	public void print() {
 		System.out.println("Print All Array Number:");
+		for(int i=0;i<100;i++)
+		System.out.println("Print i" + i);
+		
 		display();
 	}
 
