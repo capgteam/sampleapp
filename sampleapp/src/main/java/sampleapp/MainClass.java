e8477d8458636cf87fc73bdea3f25f8e2068fec1
capgteam <capgteam22020@gmail.com>
2021-08-17 11:13:33 +0530

    greetuser updated

--- sampleapp/src/main/java/sampleapp/MainClass.java
+++ sampleapp/src/main/java/sampleapp/MainClass.java
@@ -3,14 +3,14 @@ package sampleapp;
 public class MainClass {
 	
 	public void greetUser() {
-		
-	}
-
-	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Good AfterNoon");
 		System.out.println("Have A Good Day!");
 		System.out.println("welcome to Git !");
+	}
+
+	public static void main(String[] args) {
+		
 		
 	}
 
