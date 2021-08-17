0a85e33aa2937210c74ef4125071c0e8e39a43de
capgteam <capgteam22020@gmail.com>
2021-08-17 10:13:59 +0530

    displat method updated

--- sampleapp/src/main/java/sampleapp/SalesClass.java
+++ sampleapp/src/main/java/sampleapp/SalesClass.java
@@ -5,11 +5,11 @@ public class SalesClass {
 	public void display() {
 		int[] arr= {23,45,56,78,6};
 		int sum=0;
-		for(int num:arr) {
-			sum+=num;
-			System.out.println(num);
+		for(int i=0;i<arr.length;i++) {
+			sum+=arr[i];
+			System.out.println(arr[i]);
 		}
-		
+		System.out.println("Array Size:" + arr.length);
 		System.out.println("Summation:" + sum);
 	}
 	
